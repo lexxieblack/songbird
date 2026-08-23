7d48704de4ba79d3898a7d8491f3863a6a1a9991
Lexxie Black <lexxie.raven.black@proton.me>
2026-08-23 03:31:38 +0100

    Added models for a honeypot channel

---
+++ database/management/04.blackwall.sql
@@ -0,0 +1,10 @@
+CREATE TABLE management.blackwall (
+    guild_id            BIGINT NOT NULL,
+    channel_id          BIGINT,
+    whitelisted_roles   BIGINT[] NOT NULL DEFAULT '{}',
+    banned_count        INTEGER NOT NULL DEFAULT 0,
+    created_at          TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at          TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_management_blackwall_guild_id PRIMARY KEY (guild_id)
+);
